fe139a1bf6da4bfae6adfc60f663ea8c5ce447ec
Piet <pkorfmaker@gmail.com>
2021-01-07 10:22:49 +0100

    Added the migrations

--- public/js/app.js
+++ public/js/app.js
@@ -115,8 +115,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! f:\programming_install_dump\xampp\htdocs\boek\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! f:\programming_install_dump\xampp\htdocs\boek\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! F:\programming_install_dump\xampp\htdocs\boek\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! F:\programming_install_dump\xampp\htdocs\boek\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
